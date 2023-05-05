a3ac0417e76d37dc4839470200a8b0d76c099323
Shumpei Sekihiro <ssekihiro@outlook.jp>
2023-05-05 16:49:07 +0900

    add ABC294.a

---
+++ ABC294/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    //vector<int> A(n);
+
+    for (int i=0; i<n; i++){
+        int a;
+        cin >> a;
+
+        if (a % 2 == 0){
+            cout << a << " ";
+        }
+    }
+    cout << endl;
+
+
+
+
+}
