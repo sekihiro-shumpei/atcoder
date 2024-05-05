4a08cf61624731c8b34ca6b3ff2dc6a8723f8181
Shumpei Sekihiro <ssekihiro@outlook.jp>
2024-05-05 15:37:57 +0900

    add

---
+++ ABC343/A.py
@@ -0,0 +1,9 @@
+A:int = 0
+B:int = 0
+
+A, B = map(int, input().split())
+
+if A != 0 or B != 0:
+  print(0)
+else:
+  print(1)
---
+++ ATDPC/A.py
@@ -0,0 +1,25 @@
+N = int(input())
+
+ps = [0] + list(map(int, input().split()))
+
+P = sum(ps)
+
+exist = []
+for i in range(N+1):
+  exist.append([False]*(P+1))
+  
+exist[0][0] = True
+
+for i in range(1, N+1):
+  for s in range(P+1):
+    if exist[i-1][s]:
+      exist[i][s] = True
+    if s >= ps[i] and exist[i-1][s-ps[i]]:
+      exist[i][s] = True
+      
+ans = 0
+for s in range(P+1):
+  if exist[N][s]:
+    ans += 1
+    
+print(ans)
